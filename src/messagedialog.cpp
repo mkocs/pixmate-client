dd1fe2fb79fe4c556997d4c13917c569a9036e4b
mkocs <mko.mko31@gmail.com>
2016-02-07 22:19:31 +0100

    Add new dialog for informing the user about any events

---
+++ src/messagedialog.cpp
@@ -0,0 +1,17 @@
+#include "src/messagedialog.h"
+#include "ui_messagedialog.h"
+
+MessageDialog::MessageDialog(QWidget *parent, QString text) :
+  QDialog(parent),
+  ui(new Ui::MessageDialog)
+{
+  ui->setupUi(this);
+  connect(ui->okButton, SIGNAL(clicked()), this, SLOT(close()));
+
+  ui->messageLabel->setText(text);
+}
+
+MessageDialog::~MessageDialog()
+{
+  delete ui;
+}
